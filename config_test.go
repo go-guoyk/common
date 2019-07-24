eba471e129a9610e4e1dc3fa82a1e7b2f5e260eb
Guo Y.K <hi@guoyk.net>
2019-07-24 15:43:16 +0800

    add LoadJSONConfig, LoadJSONConfigFile

--- config_test.go
+++ config_test.go
@@ -41,3 +41,29 @@ func TestLoadYAMLConfigFile(t *testing.T) {
 
 	t.Log(cfg)
 }
+
+func TestLoadJSONConfigFile(t *testing.T) {
+	var err error
+	var cfg TestConfig
+
+	os.Setenv("TEST_FIELD23", "val23")
+
+	if err = LoadYAMLConfigFile("testdata/config.json", &cfg); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(cfg.Field1.Field11) != 2 {
+		t.Fatal("field11 len not match")
+	}
+	if !cfg.Field2.Field21 {
+		t.Fatal("field21 not true")
+	}
+	if cfg.Field2.Field22 != "val22" {
+		t.Fatal("field22 not set")
+	}
+	if cfg.Field2.Field23 != "val23" {
+		t.Fatal("field23 not set")
+	}
+
+	t.Log(cfg)
+}
